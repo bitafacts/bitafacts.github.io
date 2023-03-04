4ad7eafe5f95aa7af54616350e5cb5a0b912cb89
Pravat <pravat2003@gmail.com>
2023-03-04 22:32:04 +0530

    all page changes added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { CareerComponent } from './career/career.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServiceComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'careers', component: CareerComponent },
   { path: '**', component: PageNotFoundComponent }
 
 ];
